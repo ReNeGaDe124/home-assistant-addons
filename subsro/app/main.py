09a8104056fcec272bd502f0e6f641dce40b9353
ReNeGaDe124 <81156980+ReNeGaDe124@users.noreply.github.com>
2026-01-06 21:29:18 +0200

    Delete subsro directory

--- subsro/app/main.py
+++
@@ -1,8 +0,0 @@
-from threading import Thread
-from web import app
-from scanner import start_scheduler
-
-print("[subsro] Starting add-on")
-
-Thread(target=start_scheduler, daemon=True).start()
-app.run(host="0.0.0.0", port=8999)
