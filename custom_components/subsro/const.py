00cc5a360daccaf9f0128a24655b4fb87e81ecde
ReNeGaDe124 <81156980+ReNeGaDe124@users.noreply.github.com>
2026-01-17 15:03:42 +0200

    Update const.py

--- custom_components/subsro/const.py
+++ custom_components/subsro/const.py
@@ -3,7 +3,7 @@ CONF_URL = "url"
 CONF_SECRET = "secret"
 
 DEFAULT_URL = "http://localhost:8999"
-DEVICE_NAME = "Subs.ro Plex Subtitles Downloader"
+DEVICE_NAME = "Subs.ro Plex Subtitle Downloader"
 
 WEBHOOK_ID = "subsro_sync_webhook"
 SIGNAL_UPDATE = "subsro_update_signal"
