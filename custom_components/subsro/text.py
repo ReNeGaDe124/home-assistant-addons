58e180df35f61433027ff0629c5f211944db21bc
ReNeGaDe124 <81156980+ReNeGaDe124@users.noreply.github.com>
2026-01-17 17:29:57 +0200

    Update text.py

--- custom_components/subsro/text.py
+++ custom_components/subsro/text.py
@@ -15,7 +15,7 @@ class SubsroSearchInput(TextEntity):
         self._config, self._entry = config, entry
         self._attr_name = "Search & Download Subtitles"
         self._attr_unique_id = "subsro_search_and_download_subtitles"
-        self._entity_id = "text.subsro_search_and_download_subtitles"
+        self.entity_id = "text.subsro_search_and_download_subtitles"
         self._attr_icon, self._attr_mode = "mdi:magnify", "text"
         self._attr_native_value = ""
 
@@ -39,4 +39,5 @@ class SubsroSearchInput(TextEntity):
         except Exception as e:
 
             _LOGGER.error("Eroare căutare: %s", e)
+
 
