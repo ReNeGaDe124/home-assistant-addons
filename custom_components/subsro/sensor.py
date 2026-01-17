b77291a7c253cc5d2c532d0ce1c2aefed7a082b7
ReNeGaDe124 <81156980+ReNeGaDe124@users.noreply.github.com>
2026-01-17 17:29:43 +0200

    Update sensor.py

--- custom_components/subsro/sensor.py
+++ custom_components/subsro/sensor.py
@@ -18,7 +18,7 @@ class SubsroStatusSensor(SensorEntity):
         self._config = config
         self._attr_name = "Status"
         self._attr_unique_id = "subsro_plex_subtitle_downloader_status"
-        self._entity_id = "sensor.subsro_plex_subtitle_downloader_status"
+        self.entity_id = "sensor.subsro_plex_subtitle_downloader_status"
         self._attr_native_value = "Idle"
         
         self._attr_should_poll = True 
@@ -86,6 +86,7 @@ class SubsroStatusSensor(SensorEntity):
     def device_info(self) -> DeviceInfo:
 
         return DeviceInfo(identifiers={(DOMAIN, self._entry.entry_id)}, name=DEVICE_NAME)
+
 
 
 
